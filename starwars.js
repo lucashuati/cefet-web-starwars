92310de8f1a1e6d4d7ee2e73e227ff1ec7aa27e3
Lucas Corrêa <lucashuati@hotmail.com>
2018-09-25 11:54:11 -0300

    Finish exercices and challenges

--- starwars.js
+++ starwars.js
@@ -1,5 +1,30 @@
-// Seu javascript aqui :)
-// Use a Star Wars API: https://swapi.co/
-// para carregar:
-//  - A lista de filmes
-//  - A introdução de cada filme, quando ele for clicado
+const toRoman = (number) => {
+  const romans = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'];
+  return romans[number - 1];
+}
+$.ajax({
+  url: 'https://swapi.co/api/films/',
+  method: 'get',
+  success: response => {
+    const sortedFilms = response.results.sort((filmA, filmB) => filmA.episode_id > filmB.episode_id);
+    sortedFilms.forEach(film => {
+      let $li = $('<li>', {
+				'data-episode-url': film.url,
+				'text': `Episode ${toRoman(film.episode_id)}: ${film.title}`
+      });
+      $("#movies ul").append($li);
+    });
+  },
+});
+
+$("#movies ul").click('li', evt => {
+  let filmUrl = $(evt.target).data('episode-url');
+  $.ajax({
+    url: filmUrl,
+    method: 'get',
+    success: response => {
+      $(".reading-animation").html(
+        `Episode ${toRoman(response.episode_id)}\n${response.title.toUpperCase()}\n\n${response.opening_crawl}`);
+    }
+  });
+});
